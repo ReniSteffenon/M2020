0e86e5306c788cb312f0a54753a95ac6ec108257
Reni Steffenon <renisteffenon@hotmail.com>
2020-10-17 20:28:05 -0300

    Implementação bem sucedida do problema L

--- M2020/src/m2020/pl/Main.java
+++ M2020/src/m2020/pl/Main.java
@@ -5,6 +5,10 @@
  */
 package m2020.pl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author renisteffenon
@@ -14,8 +18,31 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+        
+        int numero;
+        
+        numero = Integer.parseInt(in.readLine());
+        
+        String[][] entrada = new String[numero][2];
+        
+        int carrinho = 0;
+        int boneca = 0;
+        
+        for(int i =0; i<numero; i++){
+            String aux[] = in.readLine().split(" ");
+            if(aux[1].trim().equalsIgnoreCase("F")){
+                boneca++;
+            }
+            if(aux[1].trim().equalsIgnoreCase("M")){
+                carrinho++;
+            }
+        }
+        
+        System.out.println(carrinho+" carrinhos");
+        System.out.println(boneca+" bonecas");
     }
     
 }
