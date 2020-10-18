2f318978c17e56158889969a6af87de5f4a1325f
Reni Steffenon <renisteffenon@hotmail.com>
2020-10-17 22:35:48 -0300

    Implementado parcial O

--- M2020/src/m2020/po/Main.java
+++ M2020/src/m2020/po/Main.java
@@ -8,43 +8,159 @@ package m2020.po;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Wendriu
  */
 public class Main {
+
     public static void main(String[] args) throws IOException {
         InputStreamReader ir = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(ir);
-        
+
         String coluna;
-        
-        coluna = in.readLine();
-        
-        int A = 1;
-        int B = 2;
-        int C = 3;
-        int D = 3;
-        int E = 3;
-        int F = 3;
-        int G = 3;
-        int H = 3;
-        int I = 3;
-        int J = 3;
-        int K = 3;
-        
-                
-        int maior = Integer.parseInt(array[0]);
-        
-        for (int i=0; i< qtd; i++){
-            if (maior<Integer.parseInt(array[i])){
-                maior = Integer.parseInt(array[i]);
+        boolean permanece = true;
+        List<String> en = new ArrayList<String>();
+        do {
+            coluna = in.readLine();
+            if (coluna.equalsIgnoreCase("EOF")) {
+                permanece = false;
+                en.add(coluna);
+            } else {
+                en.add(coluna);
             }
-        } 
-        
-        System.out.println(maior + 1);
-        
-        
+
+        } while (permanece);
+
+        permanece = true;
+
+        int y = 0;
+
+        do {
+            int valorF = 0;
+            coluna = en.get(y);
+            if (coluna.equalsIgnoreCase("EOF")) {
+                permanece = false;
+            } else {
+                if (coluna.length() > 3) {
+                    System.out.println("Essa coluna nao existe Tobias!");
+                } else {
+                    for (int i = 0; i < coluna.length(); i++) {
+                        int valor = 0;
+                        char entrada = coluna.charAt(i);
+                        if (entrada == 'A') {
+                            valor += 1;
+                        }
+                        if (entrada == 'B') {
+                            valor += 2;
+                        }
+                        if (entrada == 'C') {
+                            valor += 3;
+                        }
+                        if (entrada == 'D') {
+                            valor += 4;
+                        }
+                        if (entrada == 'E') {
+                            valor += 5;
+                        }
+                        if (entrada == 'F') {
+                            valor += 6;
+                        }
+                        if (entrada == 'G') {
+                            valor += 7;
+                        }
+                        if (entrada == 'H') {
+                            valor += 8;
+                        }
+                        if (entrada == 'I') {
+                            valor += 9;
+                        }
+                        if (entrada == 'J') {
+                            valor += 10;
+                        }
+                        if (entrada == 'K') {
+                            valor += 11;
+                        }
+                        if (entrada == 'L') {
+                            valor += 12;
+                        }
+                        if (entrada == 'M') {
+                            valor += 13;
+                        }
+                        if (entrada == 'N') {
+                            valor += 14;
+                        }
+                        if (entrada == 'O') {
+                            valor += 15;
+                        }
+                        if (entrada == 'P') {
+                            valor += 16;
+                        }
+                        if (entrada == 'Q') {
+                            valor += 17;
+                        }
+                        if (entrada == 'R') {
+                            valor += 18;
+                        }
+                        if (entrada == 'S') {
+                            valor += 19;
+                        }
+                        if (entrada == 'T') {
+                            valor += 20;
+                        }
+                        if (entrada == 'U') {
+                            valor += 21;
+                        }
+                        if (entrada == 'V') {
+                            valor += 22;
+                        }
+                        if (entrada == 'W') {
+                            valor += 23;
+                        }
+                        if (entrada == 'X') {
+                            valor += 24;
+                        }
+                        if (entrada == 'Y') {
+                            valor += 25;
+                        }
+                        if (entrada == 'Z') {
+                            valor += 26;
+                        }
+
+                        if (coluna.length() == 1) {
+
+                        }
+                        if (coluna.length() == 2) {
+                            if (i == 0) {
+                                valor = valor * 26;
+                            }
+
+                        }
+                        if (coluna.length() == 3) {
+                            if (i == 0) {
+                                valor = (valor * 26) * 26;
+                            }
+                            if (i == 1) {
+                                valor = valor * 26;
+                            }
+
+                        }
+                        valorF += valor;
+
+                    }
+                    if (valorF > 16385) {
+                        System.out.println("Essa coluna nao existe Tobias!");
+                    } else {
+                        System.out.println(valorF);
+                    }
+                }
+
+            }
+            y++;
+        } while (permanece);
+
     }
 }
